7a850d10c54e598c73bf667684431526d7fc0d35
jikwan <jikwan0327@gmail.com>
2024-07-12 04:25:58 +0900

    feat: 로그인 쿼리스트링

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,5 +1,6 @@
 import axios, { type AxiosError } from "axios";
 import { Cookies } from "react-cookie";
+import { useSearchParams } from "react-router-dom";
 
 export const instance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
@@ -23,11 +24,12 @@ instance.interceptors.request.use(
 instance.interceptors.response.use(
   response => response,
   async (error: AxiosError<AxiosError>) => {
+    const [searchParams] = useSearchParams();
     if (axios.isAxiosError(error) && error.response) {
       if (error.response.status === 401 || !cookies.get("access_token")) {
         cookies.remove("access_token");
         cookies.remove("refresh_token");
-        window.location.href = "/signin";
+        window.location.href = `/signin?id=${searchParams.get("id")}`;
       }
     }
     throw error;
--- src/apis/user/index.ts
+++ src/apis/user/index.ts
@@ -1,7 +1,7 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { instance, type SignInRequest } from "@/apis";
 import toast from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import axios from "axios";
 import { Cookies } from "react-cookie";
 
@@ -10,13 +10,14 @@ const cookie = new Cookies();
 
 export const useSignIn = (data: SignInRequest) => {
   const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
 
   return useMutation({
     mutationFn: () => axios.post(`${import.meta.env.VITE_BASE_URL}${router}/login`, data),
     onSuccess: res => {
       cookie.set("access_token", res.data.token);
       toast.success("로그인에 성공하였어요.");
-      navigate("/");
+      navigate(`/?id=${searchParams.get("id")}`);
     },
     onError: () => {
       toast.error("로그인에 실패했어요.");
