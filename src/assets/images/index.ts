5c9b93c7037563e1e940830c3ec8ae7feeaedb50
JJIIIINN <kgb21537@gmail.com>
2024-07-11 22:03:31 +0900

    feat: common 컴포넌트

---
+++ src/assets/images/index.ts
@@ -0,0 +1,7 @@
+export { default as Logo } from "./logo.svg";
+export { default as Search } from "./search.svg";
+export { default as Plus } from "./plus.svg";
+export { default as Minus } from "./minus.svg";
+
+export { default as DSM } from "./dsm.png";
+export { default as DSMLogo } from "./dsmLogo.png";
