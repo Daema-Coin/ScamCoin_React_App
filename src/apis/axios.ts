eb37c4616e1d6e8db3a1e65e4d2a1b06f2b2c7ba
jikwan <jikwan0327@gmail.com>
2024-07-12 04:39:52 +0900

    fix: build 오류 수정

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,7 +1,5 @@
 import axios, { type AxiosError } from "axios";
-import { Console } from "console";
 import { Cookies } from "react-cookie";
-import { useSearchParams } from "react-router-dom";
 
 export const instance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
@@ -26,10 +24,8 @@ instance.interceptors.response.use(
   response => response,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   async (error: any) => {
-    console.log(123123);
     if (axios.isAxiosError(error) && error.response) {
       if (error.response.data.detail === "Missing Authorization Header" || !cookies.get("access_token")) {
-        console.log(error.response);
         cookies.remove("access_token");
         cookies.remove("refresh_token");
         window.location.href = `/signin?id=${new URLSearchParams(window.location.search).get("id")}`;
